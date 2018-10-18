ecb3dbf6c38f74a3c4ac668b5d0490c76f945020
Jeremy White <patch3s93@gmail.com>
2018-10-18 11:42:48 -0400

    Everything works

--- roman/roman_code.rb
+++ roman/roman_code.rb
@@ -1,96 +1,96 @@
 def letters()
   roman_stuff = {
-  	1000 => "M",
-  	900 => "CM",
-  	500 => "D",
-  	400 => "CD",
-  	100 => "C",
-  	90 => "XC",
-  	50 => "L",
-  	40 => "XL",
-  	10 => "X",
-  	9 => "IX",
-  	5 => "V",
-  	4 => "IV",
-  	1 => "I"
+    1000 => "M",
+    900 => "CM",
+    500 => "D",
+    400 => "CD",
+    100 => "C",
+    90 => "XC",
+    50 => "L",
+    40 => "XL",
+    10 => "X",
+    9 => "IX",
+    5 => "V",
+    4 => "IV",
+    1 => "I"
   }
   return roman_stuff
 end
 
 def arabic()
-	arabic_stuff = {
-	"M" => 1000,
-	"CM" => 900,
-	"D" => 500,
-	"CD" => 400,
-	"C" => 100,
-	"XC" => 90,
-	"L" => 50,
-	"XL" => 40,
-	"X" => 10,
-	"IX" => 9,
-	"V" => 5,
-	"IV" => 4,
-	"I" => 1
+  arabic_stuff = {
+    "M" => 1000,
+    "CM" => 900,
+    "D" => 500,
+    "CD" => 400,
+    "C" => 100,
+    "XC" => 90,
+    "L" => 50,
+    "XL" => 40,
+    "X" => 10,
+    "IX" => 9,
+    "V" => 5,
+    "IV" => 4,
+    "I" => 1
   }
+  return arabic_stuff
 end
 
 def roman_converter(number)
   number = number
-  p number
   answer = ""
   letters().each do |key, value|
-  	unless number < key
-  		stuff = number / key
-  		things = number % key
-  		if stuff > 0
-  			stuff.times do
-  				answer << value
-  			end
-  			number = things
-  		end
-  	end
+    unless number < key
+      stuff = number / key
+      things = number % key
+      if stuff > 0
+        stuff.times do
+          answer << value
+        end
+        number = things
+      end    
+    end  
   end
   return answer
 end
 
-def arabic_converter(numeral)
-	numeral = numeral
-	numeral_array = numeral.split('')
-	counter = 0
-	numeral_array.each do|element|
-		unless numeral_array[counter].class == Integer
-		case element
-		when "C"
-		if numeral_array[counter + 1] == "M"
-          numeral_array[counter] = "CM"
-          numeral_array[counter + 1] = 0
-        elsif numeral_array[counter + 1] == "D"
-          numeral_array[counter] = "CD"
-          numeral_array[counter + 1] = 0   
+def arabic_converter(numerals)
+  numerals = numerals
+  numerals_array = numerals.split('')
+  counter = 0
+  numerals_array.each do |element|
+    unless numerals_array[counter].class == Integer
+      case element
+      when "C"
+        if numerals_array[counter + 1] == "M"
+          numerals_array[counter] = "CM"
+          numerals_array[counter + 1] = 0
+        elsif numerals_array[counter + 1] == "D"
+          numerals_array[counter] = "CD"
+          numerals_array[counter + 1] = 0   
         end
       when "X"
-        if numeral_array[counter + 1] == "C"
-          numeral_array[counter] = "XC"
-          numeral_array[counter + 1] = 0
-        elsif numeral_array[counter + 1] == "L"
-          numeral_array[counter] = "XL"
-          numeral_array[counter + 1] = 0   
+        if numerals_array[counter + 1] == "C"
+          numerals_array[counter] = "XC"
+          numerals_array[counter + 1] = 0
+        elsif numerals_array[counter + 1] == "L"
+          numerals_array[counter] = "XL"
+          numerals_array[counter + 1] = 0   
         end 
       when "I"
-        if numeral_array[counter + 1] == "X"
-          numeral_array[counter] = "IX"
-          numeral_array[counter + 1] = 0
-        elsif numeral_array[counter + 1] == "V"
-          numeral_array[counter] = "IV"
-          numeral_array[counter + 1] = 0   
+        if numerals_array[counter + 1] == "X"
+          numerals_array[counter] = "IX"
+          numerals_array[counter + 1] = 0
+        elsif numerals_array[counter + 1] == "V"
+          numerals_array[counter] = "IV"
+          numerals_array[counter + 1] = 0   
         end 
       end
     end
     counter += 1
   end
   final_answer = 0
-  numeral_array.each do |things|
+  numerals_array.each do |things|
     if things.class == String
       things = arabic[things]
     end
@@ -98,92 +98,3 @@ def arabic_converter(numeral)
   end                    
   return final_answer
 end
-
-
-
-
-
-
-# def numerals(numbers)
-# 	letters = %w(M,CM,D,CD,C,XC,L,XL,X,IX,V,IV,I)
-# 	values = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
-# 	empire = letters.zip values
-#  	n = numbers
-#   	numerals = "Charger"
-#   	empire.each do |v|
-#     numerals += v[0]*(n/v[1])
-#     n = n%v[1] 
-#   end
-#   return numerals
-# end
-
-# def romans(stuff)
-# 	letters = %w(CM M CD D XC C XL L IX X IV V I)
-# 	values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-# 	romans_num = letters.zip values
-# 	number = stuff.to_s
-# 	stuff = values
-# 	p number
-# 	p stuff
-# 	romans_num.times do |s|
-# 		number ! v[1] * number.scan(/#{v[0]}/)
-# 		number = number.scan(/#{v[0]}/)
-# 	end
-# 	return numerals
-# end
-
-
-
-# def romans(whatever)
-#   letters = %w(CM M CD D XC C XL L IX X IV V I)
-#   values = [900, 1000, 400, 500, 90, 100, 40, 50, 9, 10, 4, 5, 1]
-#   romans_num = letters.zip values
-#   number = whatever.to_s
-#   romans_num.each do |v|
-#     number += v[1]*number.scan(/#{v[0]}/).count
-#     number = number.scan(/#{v[0]}/, "")
-#   end
-#   return number
-# end
-
-
-
-
-
-# def numerals(arab, rom)
-#   	number_key = {
-#     1000 => "M", 900 => "CM", 500 => "D", 400 => "CD",
-#     100 => "C", 90 => "XC", 50 => "L", 40 => "XL",
-#     10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I",
-#     }
-# end
-
-# def arabic(arab, rom)
-# 	number_key = {
-#     400 => "CD", 900 => "CM", 90 => "XC", 40 => "XL", 9 => "IX",
-#     4 => "IV", 1000 => "M", 500 => "D", 100 => "C", 50 => "L",
-#     10 => "X", 5 => "V", 1 => "I"
-# 	}
-#   numeral_key = number_key.invert
-#   return numeral_key
-# end
-
-# def romanize(n, hash)
-#   if n == 0
-#   return ""
-# elsif hash == numerals
-#     roman = ""
-#     hash.each do |value, letter|
-#       roman << letter*(n / value)
-#       n = n % value
-#     end
-#     return roman
-#   elsif hash == numerals_reversed
-#     roman = 0
-#     hash.each do |value, letter|
-#       roman += letter * n.scan(/#{value}/).count()
-#       n = n.sub(/#{value}/, "")
-#     end
-#     return roman
-#   end
-# end
--- roman/roman_test.rb
+++ roman/roman_test.rb
@@ -2,25 +2,33 @@ require "minitest/autorun"
 require_relative "roman_code.rb"
 class My_test < Minitest::Test
 
-	# def test_one
-	# 	assert_equal(1,1)
-	# end
+	def test_one
+		assert_equal(1,1)
+	end
 	# def test_for_converter
 	# 	assert_equal("IV", roman_converter(4))
 	# 	assert_equal("XIX", roman_converter(19))
 	# 	assert_equal("CDX", roman_converter(410))
 	# end
-	# def test_for_bigger_numbers
-	# 	assert_equal("MCXCI", roman_converter(1191))
-	# 	assert_equal("MLXVI", roman_converter(1066))
-	# 	assert_equal("XLIX", roman_converter(49))
-	# 	assert_equal("CMXCIX", roman_converter(999))
-	# 	assert_equal("DCLXVI", roman_converter(666))
-	# end
+	def test_for_bigger_numbers
+		assert_equal("MCXCI", roman_converter(1191))
+		assert_equal("MLXVI", roman_converter(1066))
+		assert_equal("XLIX", roman_converter(49))
+		assert_equal("CMXCIX", roman_converter(999))
+		assert_equal("DCLXVI", roman_converter(666))
+	end
+	def test_for_arabic
+		assert_equal("DCLIX", roman_converter(659))
+		assert_equal("CXCIV", roman_converter(194))
+		assert_equal("MDXCI", roman_converter(1591))
+		assert_equal("CMVIII", roman_converter(908))
+		assert_equal("XCIX", roman_converter(99))
+	end
+
 	# def test_for_arabic_converter
-	# 	assert_equal()
-	# 	assert_equal(["X","I","X"], arabic_converter("XI"))
-	# 	assert_equal(4, arabic_converter("MXIIV").count)
+	# 	assert_equal("I","V", arabic_converter(4))
+	# 	assert_equal("X","I", arabic_converter(11))
+	# 	assert_equal("X","L", arabic_converter(40))
 	# end
 	def test_can_convert_numbers
 		assert_equal(11, arabic_converter("XI"))
@@ -30,26 +38,11 @@ class My_test < Minitest::Test
 		assert_equal(18, arabic_converter("XVIII"))
 	end
 	def test_that_can_convert_numbers_in_the_teens
-    assert_equal(1989, arabic_converter("MCMLXXXIX"))
-    assert_equal(1066, arabic_converter("MLXVI"))
-    assert_equal(49, arabic_converter("XLIX"))
-    assert_equal(999, arabic_converter("CMXCIX"))
-    assert_equal(667, arabic_converter("DCLXVII"))
+    	assert_equal(1989, arabic_converter("MCMLXXXIX"))
+    	assert_equal(1066, arabic_converter("MLXVI"))
+    	assert_equal(49, arabic_converter("XLIX"))
+    	assert_equal(999, arabic_converter("CMXCIX"))
+    	assert_equal(667, arabic_converter("DCLXVII"))
 	end
 
 end		
-	# end
-	# def test_for_string
-	# 	assert_equal(String, numerals(1))
-	# end
-	# def test_for_arabic
-	# 	assert_equal(String, numerals(40)
-	# end
-	# def test_for_numerals
-	# 	assert_equal(, numerals(5)
-	# end
-	# def test_for_p
-	# 	assert_equal(String,numerals,numbers(10).class)
-	# 	assert_equal(String, romans, numbers(14).class)
-	# end
-
